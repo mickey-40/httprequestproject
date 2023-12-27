7ef74b7ca6041e232c26933ef5cc3f5e3675cfdd
mickey-40 <mickeyarnold53@yahoo.com>
2023-12-27 14:17:27 -0600

    finished ajax.js so that it displays the users from users.json file

--- ajax.js
+++ ajax.js
@@ -14,7 +14,23 @@ function getUsers (e) {
 
   http.onload = function(){
     if(this.status === 200){
-      console.log(this.responseText);
+      // console.log(this.responseText);
+      const users = JSON.parse(this.responseText);
+
+      let output = "";
+      users.forEach(user => {
+        output += `
+          <hr>
+          <ul>
+            <li>ID: ${user.id}</li>
+            <li>Name: ${user.name}</li>
+            <li>Age: ${user.age}</li>
+            <li>Email: ${user.email}</li>
+          </ul>
+        `;
+      });
+
+      document.getElementById("users").innerHTML = output;
     }
   }
 
